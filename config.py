6645d313af61adc4181e81c54fe655a93ff67136
Spidz <85760686+spidzsaus@users.noreply.github.com>
2024-12-23 17:11:12 +0300

    Added config

---
+++ config.py
@@ -0,0 +1,8 @@
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+
+HOST = os.getenv('HOST')
+
--- projectmanager/settings.py
+++ projectmanager/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from config import HOST
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-2(9c0wqwyt@ucd4igf6jhh=f1wbcv+e&9k4k6d1+rd)=7lg7h-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [HOST]
 
 
 # Application definition
